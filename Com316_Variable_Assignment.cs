6ac212133950804b3ecd960a7d55e0b0089529c9
dayotunde25 <112938939+dayotunde25@users.noreply.github.com>
2024-02-13 16:41:31 +0100

    Add files via upload

---
+++ Com316_Variable_Assignment.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+namespace Solution{
+    class Solution { 
+	    static void Main(String[] args) { 
+		    int i = 4; 
+			double d = 4.0; 
+			string s = "Polyilaro ";
+			int zee;
+			double cor;
+			string texting;
+
+			zee = Convert.ToInt32(Console.ReadLine());
+		
+			cor = Convert.ToDouble(Console.ReadLine());
+	
+			texting = Console.ReadLine();
+			Console.WriteLine(i + zee);
+			Console.WriteLine(string.Format("{0:0.00}", (d + cor)));
+			Console.WriteLine(s + texting);
+		}
+	}
+}
